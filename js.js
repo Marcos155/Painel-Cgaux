586417a818f14f2340c9e5faa1cf7a02696f8d95
Marcos155 <53875288+Marcos155@users.noreply.github.com>
2023-06-27 11:09:48 -0300

    Add files via upload

--- js.js
+++ js.js
@@ -1,10 +1,20 @@
 $(".custom-carousel").owlCarousel({
-    autoWidth: true,
-    loop: true
+  autoWidth: true,
+  loop: true
+});
+$(document).ready(function () {
+  $(".custom-carousel .item").click(function () {
+    $(".custom-carousel .item").not($(this)).removeClass("active");
+    $(this).toggleClass("active");
   });
-  $(document).ready(function () {
-    $(".custom-carousel .item").click(function () {
-      $(".custom-carousel .item").not($(this)).removeClass("active");
-      $(this).toggleClass("active");
-    });
-  });
+});
+
+
+var text = 'Ford 2.0';
+var select = document.querySelector('#carros');
+for (var i = 0; i < select.options.length; i++) {
+  if (select.options[i].text === text) {
+    select.selectedIndex = i;
+    break;
+  }
+}
